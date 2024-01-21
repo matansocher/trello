83d1d55e4efead6fbc00e2c78313ae22783ce763
Matan Socher <matan.socher@zoominfo.com>
2024-01-22 00:58:25 +0200

    chore: code cleaning

--- vite.config.ts
+++ vite.config.ts
@@ -6,21 +6,13 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      // @ts-ignore
       '@assets': path.resolve(__dirname, 'src/assets/index'),
-      // @ts-ignore
       '@components': path.resolve(__dirname, 'src/components/index'),
-      // @ts-ignore
       '@constants': path.resolve(__dirname, 'src/constants/index'),
-      // @ts-ignore
       '@context': path.resolve(__dirname, 'src/context/index'),
-      // @ts-ignore
       '@hooks': path.resolve(__dirname, 'src/hooks/index'),
-      // @ts-ignore
       '@models': path.resolve(__dirname, 'src/models/index'),
-      // @ts-ignore
       '@services': path.resolve(__dirname, 'src/services/index'),
-      // @ts-ignore
       '@screens': path.resolve(__dirname, 'src/screens/index'),
     }
   },
